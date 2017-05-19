fd0053458416629d15ade1d9af160b097f87c32c
abidmmp <abidmmp99@gmail.com>
2017-05-19 10:06:00 +0500

    Fix prop validation

--- core_logic.jsx
+++ core_logic.jsx
@@ -164,3 +164,4 @@
 // Improve component generics - 2020-04-30 10:04:00
 // Improve dynamic imports - 2019-08-07 10:00:00
 // Refactor render logic - 2022-08-20 10:10:00
+// Fix prop validation - 2017-05-19 10:06:00
