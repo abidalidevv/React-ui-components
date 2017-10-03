e16f54be3dae82829c1c68daaa13b8fd6a248f2c
abidmmp <abidmmp99@gmail.com>
2017-10-03 10:08:00 +0500

    Fix child component update

--- core_logic.jsx
+++ core_logic.jsx
@@ -132,3 +132,4 @@
 // Refactor render logic - 2020-06-10 10:06:00
 // Optimize re-renders - 2022-06-15 10:02:00
 // Add default props - 2024-07-30 10:12:00
+// Fix child component update - 2017-10-03 10:08:00
