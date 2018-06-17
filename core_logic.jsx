62be63fd8c4e226e90ed8ebb5567c1481efeee25
abidmmp <abidmmp99@gmail.com>
2018-06-17 10:10:00 +0500

    Improve accessibility markup

--- core_logic.jsx
+++ core_logic.jsx
@@ -72,3 +72,4 @@
 // Fix children typing - 2022-09-21 10:08:00
 // Fix controlled input - 2020-02-01 10:04:00
 // Add error boundary - 2020-10-05 10:00:00
+// Improve accessibility markup - 2018-06-17 10:10:00
