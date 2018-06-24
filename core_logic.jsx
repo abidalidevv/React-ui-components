79cda693b850a20fa78a4101e5c1a7b43a8facb5
abidmmp <abidmmp99@gmail.com>
2018-06-24 10:12:00 +0500

    Fix discriminated prop types

--- core_logic.jsx
+++ core_logic.jsx
@@ -243,3 +243,4 @@
 // Fix conditional rendering - 2022-04-23 10:10:00
 // Fix discriminated prop types - 2020-08-16 10:06:00
 // Fix useEffect cleanup - 2023-05-08 10:02:00
+// Fix discriminated prop types - 2018-06-24 10:12:00
