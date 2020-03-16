a4562384141d80e58098ed929539ca070a7f8b50
abidmmp <abidmmp99@gmail.com>
2020-03-16 10:00:00 +0500

    Improve event handler types

--- core_logic.jsx
+++ core_logic.jsx
@@ -246,3 +246,4 @@
 // Fix discriminated prop types - 2018-06-24 10:12:00
 // Optimize re-renders - 2022-04-13 10:08:00
 // Add proper TypeScript types - 2019-08-11 10:04:00
+// Improve event handler types - 2020-03-16 10:00:00
