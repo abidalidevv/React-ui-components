81883e4ff6bd2a7186439388f03a1cac55427809
abidmmp <abidmmp99@gmail.com>
2020-07-02 10:08:00 +0500

    Improve dynamic imports

--- core_logic.jsx
+++ core_logic.jsx
@@ -195,3 +195,4 @@
 // Fix conditional rendering - 2019-07-07 10:06:00
 // Refactor styled component types - 2020-06-21 10:02:00
 // Add missing key prop - 2024-09-18 10:12:00
+// Improve dynamic imports - 2020-07-02 10:08:00
