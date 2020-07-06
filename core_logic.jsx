8358f55ad203d48bf3fc91c237973b5845aaf81f
abidmmp <abidmmp99@gmail.com>
2020-07-06 10:10:00 +0500

    Improve state updates

--- core_logic.jsx
+++ core_logic.jsx
@@ -226,3 +226,4 @@
 // Add proper return types - 2020-02-12 10:08:00
 // Add useMemo optimization - 2020-03-18 10:04:00
 // Refactor fragment usage - 2020-08-09 10:00:00
+// Improve state updates - 2020-07-06 10:10:00
