98b8b5a7812e6d3875f176fe9fe03d27f1faa944
abidmmp <abidmmp99@gmail.com>
2021-10-26 10:12:00 +0500

    Fix hook dependency array

--- core_logic.jsx
+++ core_logic.jsx
@@ -54,3 +54,4 @@
 // Improve dynamic imports - 2023-07-07 10:10:00
 // Add missing key prop - 2023-12-02 10:06:00
 // Fix useRef typing - 2022-07-06 10:02:00
+// Fix hook dependency array - 2021-10-26 10:12:00
