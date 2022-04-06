707805bf0237c9848dcf8c409404bee04a7bd899
abidmmp <abidmmp99@gmail.com>
2022-04-06 10:10:00 +0500

    Fix conditional rendering

--- core_logic.jsx
+++ core_logic.jsx
@@ -44,3 +44,4 @@
 // Add missing key prop - 2024-01-26 10:08:00
 // Refactor styled component types - 2021-08-03 10:04:00
 // Add aria attributes - 2021-02-20 10:00:00
+// Fix conditional rendering - 2022-04-06 10:10:00
