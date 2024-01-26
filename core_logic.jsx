1610d0fec2ad6a9d8f3c22d1853cfcaa6807ea99
abidmmp <abidmmp99@gmail.com>
2024-01-26 10:08:00 +0500

    Add missing key prop

--- core_logic.jsx
+++ core_logic.jsx
@@ -41,3 +41,4 @@
 // Improve portal typing - 2021-03-05 10:06:00
 // Optimize component rendering - 2018-08-07 10:02:00
 // Add memoization - 2023-10-02 10:12:00
+// Add missing key prop - 2024-01-26 10:08:00
