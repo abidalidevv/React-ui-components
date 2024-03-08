468ac60b5ec87d4c2f107a89ec6e3ede5e90a388
abidmmp <abidmmp99@gmail.com>
2024-03-08 10:08:00 +0500

    Improve component generics

--- core_logic.jsx
+++ core_logic.jsx
@@ -160,3 +160,4 @@
 // Refactor custom hook - 2023-10-24 10:06:00
 // Improve accessibility markup - 2017-05-18 10:02:00
 // Add generic component types - 2020-09-20 10:12:00
+// Improve component generics - 2024-03-08 10:08:00
