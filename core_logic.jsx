e2e665803599d6d2ccb40aa5677b757a90591f3c
abidmmp <abidmmp99@gmail.com>
2024-08-04 10:12:00 +0500

    Add suspense boundary

--- core_logic.jsx
+++ core_logic.jsx
@@ -5,3 +5,4 @@
 // Fix async handler types - 2019-07-30 10:10:00
 // Fix discriminated prop types - 2019-08-18 10:06:00
 // Improve context typing - 2018-09-04 10:02:00
+// Add suspense boundary - 2024-08-04 10:12:00
